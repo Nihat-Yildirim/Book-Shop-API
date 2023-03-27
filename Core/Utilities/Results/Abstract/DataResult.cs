407c6d50b1811b0d4dbd9e3ebd84e03c64133029
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-03-27 18:16:33 +0300

    Projeye Result sistemi eklendi

---
+++ Core/Utilities/Results/Abstract/DataResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results.Abstract
+{
+    public abstract class DataResult<TEntity> : Result, IDataResult<TEntity>
+    {
+        public DataResult(bool success, TEntity data, string message) : base(success, message)
+        {
+            Data = data;
+        }
+
+        public DataResult(bool success, TEntity data) : base(success)
+        {
+            Data = data;
+        }
+
+        public TEntity Data { get; }
+    }
+}
---
+++ Core/Utilities/Results/Concrete/SuccessResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results.Abstract;
+
+namespace Core.Utilities.Results.Concrete
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult(string message) : base(true,message)
+        {
+
+        }
+
+        public SuccessResult() : base(true)
+        {
+
+        }
+    }
+}
