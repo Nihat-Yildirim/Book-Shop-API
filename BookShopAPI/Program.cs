88caef8086a817e6dc4bab5fef6be03526fe8c36
Nihat-Yildirim <nihat.yilldirim@gmail.com>
2023-03-27 22:48:52 +0300

    Projeye AOP altyapısı yazıldı

--- BookShopAPI/Program.cs
+++ BookShopAPI/Program.cs
@@ -1,20 +1,40 @@
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = tokenOptions.Issuer,
+            ValidAudience = tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+        };
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- DataAccess/Concrete/EntityFramework/Context/BookShopContext.cs
+++ DataAccess/Concrete/EntityFramework/Context/BookShopContext.cs
@@ -1,4 +1,5 @@
-﻿using Core.Utilities.Configurations;
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Configurations;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,40 @@ namespace DataAccess.Concrete.EntityFramework.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("@"+Connection.ConnectionString);
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Dealer> Dealers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(c =>
+            {
+                c.ToTable("Customers").HasKey(c => c.Id);
+                c.Property(p => p.Id).HasColumnName("Id");
+                c.Property(p => p.FirstName).HasColumnName("FirstName");
+                c.Property(p => p.LastName).HasColumnName("LastName");
+                c.Property(p => p.Email).HasColumnName("Email");
+                c.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+                c.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+                c.Property(p => p.ProfilePicture).HasColumnName("ProfilePicture");
+                c.Property(p => p.CreatedDate).HasColumnName("CreatedDate");
+                c.Property(p => p.Status).HasColumnName("Status");
+            });
+
+            modelBuilder.Entity<Dealer>(d =>
+            {
+                d.ToTable("Dealers").HasKey(d => d.Id);
+                d.Property(p => p.Id).HasColumnName("Id");
+                d.Property(p => p.StoreId).HasColumnName("StoreId");
+                d.Property(p => p.FirstName).HasColumnName("FirstName");
+                d.Property(p => p.LastName).HasColumnName("LastName");
+                d.Property(p => p.Email).HasColumnName("Email");
+                d.Property(p => p.PasswordHash).HasColumnName("PasswordHash");
+                d.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt");
+                d.Property(p => p.CreatedDate).HasColumnName("CreatedDate");
+                d.Property(p => p.Status).HasColumnName("Status");
+            });
         }
     }
 }
